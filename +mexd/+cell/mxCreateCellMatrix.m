cc7022b72a4e7997b4d92797866423095a5964ec
Jim Hokanson <jim.hokanson@gmail.com>
2022-01-14 19:42:00 -0600

    wtf

---
+++ +mexd/+cell/mxCreateCellMatrix.m
@@ -0,0 +1,12 @@
+function mxCreateCellMatrix()
+%
+%   mxArray *mxCreateCellMatrix(mwSize m, mwSize n);
+%
+%   Example
+%   ----------------------------------------
+%   mxArray *temp = mxCreateCellMatrix(1,4);
+%   //TODO: Show population of the cells
+%
+
+
+end
---
+++ doc_sources.m
@@ -0,0 +1,2 @@
+https://www.mathworks.com/help/matlab/cc-matrix-library-api.html
+https://www.mathworks.com/help/matlab/mex-library.html
